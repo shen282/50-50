b2d1a557cac32c26ad938f0dfa9effa91e5c5baa
shen282 <shen282@purdue.edu>
2018-04-23 02:27:52 -0400

    register functioning

--- logger.js
+++ logger.js
@@ -4,7 +4,13 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
     document.getElementById("loggedIn").style.display = "block";
+    document.getElementById("login").style.display = "none";
     document.getElementById("mainLogin").style.display = "none";
+    var user = firebase.auth().currentUser;
+    if(user != null){
+      var email = user.email;
+      document.getElementById("signInText").innerHTML = "Signed in as: " + email;
+    }
   } else {
     // No user is signed in.
     document.getElementById("loggedIn").style.display = "none";
@@ -12,7 +18,9 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
-
+function matchRegular(){
+  //window.alert("button check");
+}
 
 function sendLogin(){
   var userName1 = document.getElementById("loginUsername").value;
@@ -27,8 +35,18 @@ function sendLogin(){
 });
 }
 
-/*function sendRegister(){
+function signOff(){
+   firebase.auth().signOut();
+}
+
+function sendRegister(){
   var userName1 = document.getElementById("loginUsername").value;
   var password1 = document.getElementById("loginPassword").value;
-  window.alert(userName1 + " " + password1);
-}*/
+  firebase.auth().createUserWithEmailAndPassword(userName1, password1).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  window.alert("Error: " + errorMessage);
+  // ...
+});
+}
