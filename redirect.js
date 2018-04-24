66e06f6faee2a40579519fff1009a75c113f964c
shen282 <shen282@purdue.edu>
2018-04-24 08:23:11 -0400

    Delete redirect.js

--- redirect.js
+++
@@ -1,9 +0,0 @@
-function playAgainAi(){
-  window.location.href="basicGame.html";
-}
-function playHuman() {
-  window.location.href="basicMatch.html";
-}
-function goHome(){
-  window.location.href="main.html";
-}
