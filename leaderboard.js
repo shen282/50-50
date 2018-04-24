a5e2cf30e39be9218bcdb8d0f617d71a8124b5dd
shen282 <shen282@purdue.edu>
2018-04-24 13:07:07 -0400

    Functioning leaderboard readded

--- leaderboard.js
+++ leaderboard.js
@@ -1,54 +1,85 @@
-/*import { Injectable } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import { AngularFireAuth } from 'angularfire2/auth';
 
+function updateleaderAI(){
 
-export class Item {
-  body: string;
-}
-
-
-@Injectable()
-export class ItemService {
-
-  items: FirebaseListObservable<Item[]> = null;
-  userId: string;
-
-  constructor(private db: AngularFireDatabase, private afAuth: AngularFireAuth) {
-    this.afAuth.authState.subscribe(user => {
-      if(user) this.userId = user.uid
-    })
-  }
-
-
-  // Return an observable list with optional query
-  // You will usually call this from OnInit in a component
-  getItemsList(): FirebaseListObservable<Item[]> {
-    if (!this.userId) return;
-    this.items = this.db.list(`items/${this.userId}`);
-    return this.items
-  }
-
-
-  createItem(item: Item)  {
-    this.items.push(item)
-  }
-
-}*/
-var database = firebase.database();
-
-var textIn = document.getElementById("testB");
-var text = "h";
-
-function testBoard(){
-  window.alert("hi");
+  //window.alert(uID);
   //document.getElementById("testId").innerHTML = "userid&email " + uID + email;
-  firebase.database().ref().child("users/"+uID).set({score: 100});
+  var scores = firebase.database().ref("users");
+  var counter = 9;
+  var namer;
+  var scorer;
+  //var counter2 = 15;
+  scores.orderByChild("score").limitToLast(11).on("child_added", function(snapshot) {
 
+    var holder;
+    holder = snapshot.key;
+    var u = snapshot.val();
+    //window.alert(counter + "counter");
+    if(counter == 9){
+      document.getElementById("name7").innerHTML= holder;
+      document.getElementById("score7").innerHTML = u.score;
+    }else if(counter == 8){
+      document.getElementById("name6").innerHTML= holder;
+      document.getElementById("score6").innerHTML = u.score;
+    }else if(counter == 7){
+      document.getElementById("name5").innerHTML= holder;
+      document.getElementById("score5").innerHTML = u.score;
+    }else if(counter == 6){
+      document.getElementById("name4").innerHTML= holder;
+      document.getElementById("score4").innerHTML = u.score;
+    }else if(counter == 5){
+      document.getElementById("name3").innerHTML= holder;
+      document.getElementById("score3").innerHTML = u.score;
+    }else if(counter == 4){
+      document.getElementById("name2").innerHTML= holder;
+      document.getElementById("score2").innerHTML = u.score;
+    }else if(counter == 3){
+      //window.alert("counter2if")
+      document.getElementById("name1").innerHTML= holder;
+      document.getElementById("score1").innerHTML = u.score;
+    }else if(counter == 2){
+
+    }
+
+    counter=counter-1;
+    //window.alert(counter);
+    //counter2--;
+    //window.alert(holder.email);
+    //console.log(holder);
+  });
 
 }
+function printHtml(user, scores, counter){
+  if(counter ==8){
+    document.getElementById("name1").innerHTML= holder;
+    document.getElementById("score1").innerHTML = u.score;
+  }else if(counter == 7){
+    document.getElementById("name2").innerHTML= holder;
+    document.getElementById("score2").innerHTML = u.score;
+  }else if(counter == 6){
+    document.getElementById("name3").innerHTML= holder;
+    document.getElementById("score3").innerHTML = u.score;
+  }else if(counter == 5){
+    document.getElementById("name4").innerHTML= holder;
+    document.getElementById("score4").innerHTML = u.score;
+  }else if(counter == 4){
+    document.getElementById("name5").innerHTML= holder;
+    document.getElementById("score5").innerHTML = u.score;
+  }else if(counter == 3){
+    document.getElementById("name6").innerHTML= holder;
+    document.getElementById("score6").innerHTML = u.score;
+  }else if(counter == 2){
+    document.getElementById("name7").innerHTML= holder;
+    document.getElementById("score7").innerHTML = u.score;
+  }else if(counter == 1){
+    document.getElementById("name8").innerHTML= holder;
+    document.getElementById("score8").innerHTML = u.score;
+  }
+}
+
 
 function testBoard2(){
   window.alert(uID);
-  firebase.database().ref().child("users/"+uID).set({score: 200});
+
+  //firebase.database().ref().child("users/"+uID).set({score: 150});
+  //firebase.database().ref().child("users/"+uID).set({score: 200});
 }
